455325930520182f3b246620b351b5b981603409
LarsJoachim <43725591+LarsJoachim@users.noreply.github.com>
2019-10-22 22:33:17 +0200

    Lagt til bruker og LagNyBruker-klassene

---
+++ src/main/java/Bruker.java
@@ -0,0 +1,7 @@
+public class Bruker {
+
+    String etterNavn;
+    String fornavn;
+    int alder;
+
+}
---
+++ src/main/java/LagNyBruker.java
@@ -0,0 +1,62 @@
+public class LagNyBruker {
+
+    Bruker bruker;
+    String brukernavn, passord, epost;
+    Boolean brukerErKlubb;
+
+
+    public LagNyBruker(Bruker bruker, String brukernavn, String passord, String epost, Boolean brukerErKlubb){
+
+        this.bruker = bruker;
+        this.brukerErKlubb = brukerErKlubb;
+        this.brukernavn = brukernavn;
+        this.passord = passord;
+        this.epost = epost;
+    }
+
+    public String getBrukernavn(){
+        return brukernavn;
+    }
+
+    public String getEpost(){
+        return epost;
+    }
+
+
+    public Bruker getBruker(){
+        return bruker;
+    }
+
+    public Boolean getBrukerErKlubb(){
+        return brukerErKlubb;
+    }
+
+    public void setBrukerNavn(String brukernavn){
+        this.brukernavn = brukernavn;
+    }
+
+    public void setPassord(String passord){
+        this.passord = passord;
+    }
+
+    public void setEpost(String epost){
+
+        if (epost.contains("@")){
+            this.epost = epost;
+        }
+        else{
+            System.out.println("Ugyldig epost");
+        }
+
+    }
+
+    public void setBruker(Bruker bruker){
+        this.bruker = bruker;
+    }
+
+    public void setBrukerErKlubb(Boolean brukerErKlubb){
+        this.brukerErKlubb = brukerErKlubb; 
+    }
+
+
+}
