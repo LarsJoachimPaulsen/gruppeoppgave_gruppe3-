cff32b90449b4461d4b21473823c91e7e49e4270
Robert Alexander Dankertsen <dankertsenalexander@gmail.com>
2019-10-23 15:51:37 +0200

    Fikset LagNyBrukerTest

--- src/test/LagNyBrukerTest.java
+++ src/test/LagNyBrukerTest.java
@@ -5,6 +5,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class LagNyBrukerTest {
 
-    LagNyBruker lagNyBruker = new LagNyBruker("Fornavn", "Etternavn", "abc@123.xyz", 42, "FE", "abc123", false);
+    private LagNyBruker lagNyBruker = new LagNyBruker("","","",42,"","",false);
+
+    @Test
+    void sjekkerBrukerAlder() {
+        assertEquals(42,lagNyBruker.getAlder());
+    }
 
 }
