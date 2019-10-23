17b536b05bf7771c73aa2ebc52e0d9736df74a88
LarsJoachim <43725591+LarsJoachim@users.noreply.github.com>
2019-10-23 13:16:40 +0200

    Fikset pakker 2.0

---
+++ src/main/java/no/hiof/gruppe3/LagNyBruker.java
@@ -0,0 +1,78 @@
+package no.hiof.gruppe3;
+
+public class LagNyBruker extends Bruker {
+
+    private Bruker bruker;
+    private String brukernavn, passord, epost;
+    private Boolean brukerErKlubb;
+
+
+    public LagNyBruker(
+            String fornavn, String etternavn, int alder,
+            Bruker bruker, String brukernavn, String passord, String epost, Boolean brukerErKlubb
+
+    ){
+        super(fornavn, etternavn, alder);
+        this.bruker = bruker;
+        this.brukerErKlubb = brukerErKlubb;
+        this.brukernavn = brukernavn;
+        this.passord = passord;
+        this.epost = epost;
+
+    }
+
+    //public LagNyBruker(){}
+
+
+    public void LeggTilNyBruker(Bruker bruker, String brukernavn, String passord, String epost, Boolean brukerErKlubb){
+
+    }
+
+
+    public String getBrukernavn(){
+        return brukernavn;
+    }
+
+    public String getEpost(){
+        return epost;
+    }
+
+
+    public Bruker getBruker(){
+        return bruker;
+    }
+
+    public Boolean getBrukerErKlubb(){
+        return brukerErKlubb;
+    }
+
+    public void setBrukerNavn(String brukernavn){
+        this.brukernavn = brukernavn;
+    }
+
+    public void setPassord(String passord){
+        this.passord = passord;
+    }
+
+    public void setEpost(String epost){
+
+        if (epost.contains("@")){
+            this.epost = epost;
+
+        }
+        else{
+            System.out.println("Ugyldig epost");
+        }
+
+    }
+
+    public void setBruker(Bruker bruker){
+        this.bruker = bruker;
+    }
+
+    public void setBrukerErKlubb(Boolean brukerErKlubb){
+        this.brukerErKlubb = brukerErKlubb;
+    }
+
+
+}
---
+++ src/main/java/no/hiof/gruppe3/SkrivTilServer.java
@@ -0,0 +1,31 @@
+package no.hiof.gruppe3;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public abstract class SkrivTilServer {
+
+    public void lesFraServer(){
+        String filsti = "CVC/billettinformasjon.cvc";
+        String line = "";
+        String cvsSplitBy = ",";
+
+
+        /*
+        try{
+            BufferedReader lesFraFil = new BufferedReader(new FileReader(String filsti)){
+
+                while((line = lesFraFil.readLine() != null))
+
+            }
+        }
+        catch(FileNotFoundException FNFE){
+            FNFE.printStackTrace();
+        }
+ */
+
+    }
+
+}
