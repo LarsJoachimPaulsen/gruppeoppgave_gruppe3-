ddc2d8644f3da2cd5beb31bc18a7f49d7e77663b
LarsJoachim <43725591+LarsJoachim@users.noreply.github.com>
2019-10-23 18:58:44 +0200

    Lag til test for kalkulasjon av pris

--- src/test/BestillingTest.java
+++ src/test/BestillingTest.java
@@ -1,6 +1,7 @@
 import no.hiof.gruppe3.kode.Bestilling;
 import org.junit.jupiter.api.Test;
 
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BestillingTest {
@@ -8,7 +9,7 @@ class BestillingTest {
     Bestilling bestilling = new Bestilling();
 
     @Test
-    void bestillingRedusererTilgjengligeBilleterTest(){
+    void en_bestilling_skal_redusere_antall_billetter_tiljengelig_test(){
 
         assertEquals("Det finnes desverre ikke nok billetter", bestilling.bestillBillet(103));
         assertEquals("Bestilling vellykket", bestilling.bestillBillet(100));
@@ -16,4 +17,12 @@ class BestillingTest {
 
     }
 
+    @Test
+    void pris_for_alle_billetter_valgt_er_korrekt_test(){
+
+        assertEquals(400, bestilling.kalkulerPris(2));
+        assertEquals(600, bestilling.kalkulerPris(3));
+        assertEquals(1000, bestilling.kalkulerPris(5));
+
+    }
 }
