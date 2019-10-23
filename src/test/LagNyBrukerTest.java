a4b0a8695c76fc4e1c05e9dfc67358d6f755ef23
Robert Alexander Dankertsen <dankertsenalexander@gmail.com>
2019-10-23 15:45:29 +0200

    Fikset LagNyBrukerTest

--- src/test/LagNyBrukerTest.java
+++ src/test/LagNyBrukerTest.java
@@ -1,7 +1,3 @@
-
-/*
-package no.hiof.gruppe3.kode;
-
 import no.hiof.gruppe3.kode.LagNyBruker;
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class LagNyBrukerTest {
 
-    LagNyBruker lagNyBruker = new LagNyBruker();
+    LagNyBruker lagNyBruker = new LagNyBruker("Fornavn", "Etternavn", "abc@123.xyz", 42, "FE", "abc123", false);
 
-
-} */
+}
